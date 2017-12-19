638b6f1de78e2638cfebf87403f1be646cfcb405
sam <15622749470@163.com>
2017-12-19 17:33:44 +0800

    Temporary preservation.

---
+++ LinuxC/src/cstn.c
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2017 samyorkson
+ *
+ * Use of this source code is governed by a MIT-style license 
+ * that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+
+ * File name: cstn.c
+ * Date: 2017-12-15
+ * Description: Realization of Serial Communication with Network.
+ * Note: Testing in Fedora 27.
+ */
+ 
+#include <stdio.h>      //Standard input/output definitions.
+#include <stdlib.h>     //Standard function library definition.
+#include <string.h>     //String function definitions.
+#include <unistd.h>     //UNIX standard function definitions.
+#include <fcntl.h>      //File control definitions.
+#include <termios.h>    //POSIX terminal control definitions.
+#include <errno.h>      //Error number definitions.
+#include <sys/types.h>  //Basic system data types of UNIX definitions.
+#include <sys/stat.h>   //File status UNIX definitions.
+
+int main(int argc, char *argv[])
+{
+    
+}
